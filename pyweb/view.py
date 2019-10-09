bde71ac7302b084d7843002d844ea200b5e1d4d2
drbin <dairubin0605@163.com>
2019-10-09 16:42:18 +0800

    django 模板添加

--- pyweb/view.py
+++ pyweb/view.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
+    return render(request, 'index.html')
+
+
+def exam(request):
     return render(request, 'exam.html')
+
 
 def sure(request):
     return render(request, 'sure.html')
