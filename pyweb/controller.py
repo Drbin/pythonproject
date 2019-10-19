68bea70ce6d7037879d808a48f4a8036ad151d3c
drbin <dairubin0605@163.com>
2019-10-19 15:55:17 +0800

    路径配置

--- pyweb/controller.py
+++ pyweb/controller.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 import json
 
 
@@ -8,6 +9,12 @@ def get_txt_list(request):
 
     return HttpResponse(text)
 
+
+def show_article_list(request):
+    if request.method == "GET":
+        ll = [{'name': "图书", 'title': '测试数据'}]
+        response = {'code': '200', 'msg': '查询成功', 'data': ll }
+        return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False})
 
 def show_list(request):
     return 0
