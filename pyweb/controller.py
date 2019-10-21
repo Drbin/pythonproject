d4629862dfb256d5ae89a9880702d723e339845c
drbin <dairubin0605@163.com>
2019-10-21 16:42:32 +0800

    路径配置

--- pyweb/controller.py
+++ pyweb/controller.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse
+from . import logs
 
 
 def show_txt_msg(request):
     if request.method == "GET":
         ll = {'name': "图书", 'title': '测试数据'}
         response = {'code': '200', 'msg': '查询成功', 'data': ll}
+        logs.logs_on('查询数据')
         return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False})
 
 
