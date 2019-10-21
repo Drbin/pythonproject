ee3f909b3bdba1a04f510513053ef269fab1c737
drbin <dairubin0605@163.com>
2019-10-21 16:34:40 +0800

    路径配置

---
+++ pyweb/logs.py
@@ -0,0 +1,11 @@
+import socket
+my_name = socket.getfqdn(socket.gethostname())
+my_addr = socket.gethostbyname(my_name)
+
+
+def logs_on(data):
+    f = open("logs.txt", "a+", encoding='utf-8')
+    f.write(my_addr+"\n")
+    f.write(data+"\n")
+    f.close()
+    return 0
