8b2bdc9e37e167f0c96913451a0090fb3a74a3de
drbin <dairubin0605@163.com>
2019-10-22 16:52:52 +0800

    api数据接口

--- pyweb/controller.py
+++ pyweb/controller.py
@@ -14,20 +14,14 @@ def show_article_list(request):
     if request.method == "GET":
         ll = [{'name': "图书", 'title': '测试数据'}]
         response = {'code': '200', 'msg': '查询成功', 'data': ll}
+        logs.logs_on('查询数据')
         return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False})
-
-
-def show_list(request):
-    return 0
-
-
-def show_msg():
-    return 0
 
 
 def get_list(request):
     if request.method == "GET":
         print(request.GET.get("id"))
         response = {'code': '200', 'msg': '提交成功', 'data': request.GET.get("id")}
+        logs.logs_on('获取数据')
         return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False})
 
