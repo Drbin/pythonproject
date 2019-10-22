56f37e3c31a58d3d985b3c68b02105e732e63a0e
drbin <dairubin0605@163.com>
2019-10-22 16:46:30 +0800

    日志文件修改

--- pyweb/logs.py
+++ pyweb/logs.py
@@ -1,10 +1,12 @@
 import socket
+import time
 my_name = socket.getfqdn(socket.gethostname())
 my_addr = socket.gethostbyname(my_name)
 
 
 def logs_on(data):
     f = open("logs.txt", "a+", encoding='utf-8')
+    f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+"\n")
     f.write(my_addr+"\n")
     f.write(data+"\n")
     f.close()
