a8a92c45e8254419e399a14ad3a44be557f81e1a
drbin <dairubin0605@163.com>
2019-10-24 17:46:26 +0800

    日志修改

--- pyweb/logs.py
+++ pyweb/logs.py
@@ -8,6 +8,6 @@ def logs_on(data):
     f = open("logs.txt", "a+", encoding='utf-8')
     f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+"\n")
     f.write(my_addr+"\n")
-    f.write(data+"\n")
+    f.write("["+data+"]"+"\n")
     f.close()
     return 0
