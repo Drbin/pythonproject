9341e3385c392679479c17fe156109ce20697d06
drbin <dairubin0605@163.com>
2019-10-26 14:59:10 +0800

    api数据接口

--- pyweb/de_oper.py
+++ pyweb/de_oper.py
@@ -1,24 +1,18 @@
 from django.http import HttpResponse
 
-from pyweb.models import InitDb
+from pyweb.models import AdminDb
 
-
-# 数据库操作
 def testdb(request):
     # 初始化
     response = ""
     response1 = ""
-
     # 通过objects这个模型管理器的all()获得所有数据行，相当于SQL中的SELECT * FROM
-    list = InitDb.objects.all()
+    list = AdminDb.objects.all()
 
-    InitDb.objects.order_by("id")
-
-    # 上面的方法可以连锁使用
-    InitDb.objects.filter(name="admin_tbl").order_by("tbl_admin_id")
-
+    AdminDb.objects.order_by("tbl_admin_id")
     # 输出所有数据
     for var in list:
         response1 += var.name + " "
     response = response1
+    print(list)
     return HttpResponse("<p>" + response + "</p>")
