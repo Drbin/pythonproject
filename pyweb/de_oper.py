8ee5261bd268bf0cdee81266ce668b1615ae6ac6
drbin <dairubin0605@163.com>
2019-10-26 15:04:58 +0800

    操作日志

--- pyweb/de_oper.py
+++ pyweb/de_oper.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-
+from . import logs
 from pyweb.models import AdminDb
 
 def testdb(request):
@@ -9,7 +9,9 @@ def testdb(request):
     # 通过objects这个模型管理器的all()获得所有数据行，相当于SQL中的SELECT * FROM
     list = AdminDb.objects.all()
 
+    logs.logs_on("数据库 表tbl_admin_id 执行查操作")
     AdminDb.objects.order_by("tbl_admin_id")
+
     # 输出所有数据
     for var in list:
         response1 += var.name + " "
