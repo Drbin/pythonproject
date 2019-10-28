541652c88e2a9b37ddeae51251fd1a56fb0ea336
drbin <dairubin0605@163.com>
2019-10-28 16:03:02 +0800

    api数据接口

--- pyweb/controller.py
+++ pyweb/controller.py
@@ -1,5 +1,4 @@
 from django.http import JsonResponse
-from pyweb.models import AdminDb
 from . import logs
 
 
@@ -27,22 +26,14 @@ def get_list(request):
 
 
 def get_admin(request):
-    if request.method == "GET":
-        # 通过objects这个模型管理器的all()获得所有数据行，相当于SQL中的SELECT * FROM
-        list = AdminDb.objects.all()
-        list_admin = []
-        logs.logs_on("数据库 表tbl_admin_id 执行查操作")
-        AdminDb.objects.order_by("tbl_admin_id")
+    if request.method == "POST":
+        from . import mapper
 
-        # 输出所有数据
-        for var in list:
-            dir_admin = {}
-            dir_admin["name"] = var.name
-            dir_admin["username"] = var.u_name
-            dir_admin["password"] = var.p_word
-            dir_admin["cTime"] = var.c_time
-            list_admin.append(dir_admin)
+        # 通过objects这个模型管理器的all()获得所有数据行，相当于SQL中的SELECT * FROM
+        list_admin = mapper.re_admin_data
 
         response = {'code': '200', 'msg': '提交成功', 'data': list_admin}
         return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False})
-
+    if request == "GET":
+        response = {'code': 'error', 'msg': '请求参数有误，method = POST'}
+        return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False})
